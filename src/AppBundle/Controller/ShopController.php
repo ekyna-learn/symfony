e31f8e6c749911d1c08c8723406d7e1b6eb8ca78
Etienne Dauvergne <contact@ekyna.com>
2017-01-28 18:16:20 +0100

    Added category and product routes, controllers and templates.

--- src/AppBundle/Controller/ShopController.php
+++ src/AppBundle/Controller/ShopController.php
@@ -19,4 +19,37 @@ class ShopController extends Controller
     {
         return $this->render('AppBundle:Shop:index.html.twig');
     }
+
+    /**
+     * Shop category action.
+     *
+     * @param string $categorySlug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function categoryAction($categorySlug)
+    {
+        // TODO Fetch category by slug
+
+        // TODO Fetch products by category
+
+        return $this->render('AppBundle:Shop:category.html.twig');
+    }
+
+    /**
+     * Shop product action.
+     *
+     * @param string $categorySlug
+     * @param string $productSlug
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function productAction($categorySlug, $productSlug)
+    {
+        // TODO Fetch category by slug
+
+        // TODO Fetch product by slug
+
+        return $this->render('AppBundle:Shop:product.html.twig');
+    }
 }
